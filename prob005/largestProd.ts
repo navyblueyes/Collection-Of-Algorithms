109af98deaed47dd51a2ed8dfbfac5470a4c5558
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-04-21 13:51:23 -0700

    prob005 finished

---
+++ prob005/largestProd.ts
@@ -0,0 +1,24 @@
+// Explanation ---
+//    takes input array of numbers
+//    finds the largets product of two numbers
+
+// Plan
+//    check input for less than 1 input
+//    for loop...
+//    check for negative and break
+
+function largestProduct(inputArray: number[]): number {
+  if (inputArray.length <= 1) {
+    Error("Input must be an array of two or more integers")
+  }
+  let bestProduct: number = inputArray[0] * inputArray[1]
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    if (inputArray[i] <= 0 || inputArray[i + 1] <= 0) {
+      continue
+    }
+    let product = inputArray[i] * inputArray[i + 1]
+    bestProduct = bestProduct < product ? product : bestProduct
+  }
+  return bestProduct
+}
+console.log(largestProduct([2, 5, 7, 8, 12, 23]))
