cd4a7eac942fa81bdce4477e93b68017456e00ac
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-15 17:45:07 -0700

    prob 9, started

---
+++ prob009/stringSeqCheck.ts
@@ -0,0 +1,36 @@
+// given a string
+// compare individual letters to see if sequential
+
+function stringSeqCheck(str1: string, str2: string): boolean {
+  stringArray1 = str1.split("")
+  stringArray2 = str2.split("")
+}
+
+// const alphabet = {
+//   a: "b",
+//   b: "c",
+//   c: "d",
+//   d: "e",
+//   e: "f",
+//   f: "g",
+//   g: "h",
+//   h: "i",
+//   i: "j",
+//   j: "k",
+//   k: "l",
+//   l: "m",
+//   m: "n",
+//   n: "o",
+//   o: "p",
+//   p: "q",
+//   q: "r",
+//   r: "s",
+//   s: "t",
+//   t: "u",
+//   u: "v",
+//   v: "w",
+//   w: "x",
+//   x: "y",
+//   y: "z",
+//   z: "a",
+// }
