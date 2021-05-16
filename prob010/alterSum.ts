ed16f9fc90ee4c29db2b2c3cada58425b0d85b27
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-15 18:10:58 -0700

    prob010 -- think it works

--- prob010/alterSum.ts
+++ prob010/alterSum.ts
@@ -2,5 +2,17 @@
 // return array of two integers
 //      first integer -- total sum of odd index
 //      second integer -- total sum of even index
+console.log(`hellow`)
 
-function alterSum(arr: number[]): number[] {}
+function alterSum(arr: number[]): number[] {
+  let sum1: number = 0
+  let sum2: number = 0
+  for (let i = 0; i < arr.length; i + 2) {
+    sum1 = sum1 + arr[i]
+    sum2 = sum2 + arr[i + 1]
+  }
+  console.log(`${sum1} ${sum2}`)
+  return [sum1, sum2]
+}
+
+console.log(alterSum([54, 45, 84, 74, 78, 84]))
