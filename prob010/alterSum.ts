82fd04ef887f0f24d3a1f7ec062562ebfbbb5132
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-15 17:59:27 -0700

    prob010 started

---
+++ prob010/alterSum.ts
@@ -0,0 +1,6 @@
+// given array of positive integers
+// return array of two integers
+//      first integer -- total sum of odd index
+//      second integer -- total sum of even index
+
+function alterSum(arr: number[]): number[] {}
