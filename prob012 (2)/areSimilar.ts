e217c9070aa87641240fdd5ce88b92fb25ead64e
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-23 00:03:00 -0700

    prob012 complete

--- prob012 (2)/areSimilar.ts
+++
@@ -1,7 +0,0 @@
-// goal -- compare two arrays and see if elements are interchangeable
-
-function areSimilar(a: number[], b: number[]): boolean {}
-
-console.log(areSimilar([1, 2, 3], [1, 2, 3])) // should return true
-console.log(areSimilar([1, 2, 3], [2, 1, 3])) // should return false
-console.log(areSimilar([1, 2, 3], [2, 1, 1])) // should return false
--- prob012/areSimilar.ts
+++ prob012/areSimilar.ts
@@ -1,7 +1,29 @@
 // goal -- compare two arrays and see if elements are interchangeable
 
-function areSimilar(a: number[], b: number[]): boolean {}
+function areSimilar(a: number[], b: number[]): boolean {
+  // to pass the first condition
+  if (a.toString() === b.toString()) {
+    return true;
+  }
 
-console.log(areSimilar([1, 2, 3], [1, 2, 3])) // should return true
-console.log(areSimilar([1, 2, 3], [2, 1, 3])) // should return false
-console.log(areSimilar([1, 2, 3], [2, 1, 1])) // should return false
+  // check for reverse case
+  const c: number[] = [];
+  let d: number[] = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[1]) {
+      c.push(a[i]);
+      d.push(b[i]);
+    }
+  }
+  d = d.reverse();
+  if (c.length === 2 && (c.toString() === d.toString())) {
+    return true;
+  }
+
+  //
+  return false;
+}
+
+console.log(areSimilar([1, 2, 3], [1, 2, 3])); // should return true
+console.log(areSimilar([1, 2, 3], [2, 1, 3])); // should return false
+console.log(areSimilar([1, 2, 2], [2, 2, 1])); // should return true
