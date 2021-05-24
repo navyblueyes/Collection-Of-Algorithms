4458d25c51d702b4100e8c0a43bb6ac4487d2605
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-23 23:54:40 -0700

    prob014 attempted

--- prob014/arrayConversion.ts
+++ prob014/arrayConversion.ts
@@ -7,7 +7,18 @@
 //    --> second iteration returns [21, 165]
 //    --> third iteration returns  [186]
 function arrayConversion(inputArray: number[]): number[]{
+  let count = 0;
+  function oddIteration(oddArray: number[]): number[] {
+    for (let i = 0; i < oddArray.length; i+=2) {
+      oddArray
+    }
+  }
+  function evenIteration(evenArray: number[]): number[] {
+    for (let i = 0; i < evenArray.length; i+=2) {
 
+    }
+
+  }
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
