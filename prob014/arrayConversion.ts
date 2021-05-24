9314f77e054830829e0d478a8b5a9bbe34416ac9
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-23 23:55:00 -0700

    prob014 attempted

--- prob014/arrayConversion.ts
+++ prob014/arrayConversion.ts
@@ -9,15 +9,28 @@
 function arrayConversion(inputArray: number[]): number[]{
   let count = 0;
   function oddIteration(oddArray: number[]): number[] {
+    let newOddArray = [];
     for (let i = 0; i < oddArray.length; i+=2) {
-      oddArray
+      newOddArray.push(oddArray[i] + oddArray[i + 1]);
     }
+    return newOddArray;
   }
   function evenIteration(evenArray: number[]): number[] {
+    let newEvenArray = [];
     for (let i = 0; i < evenArray.length; i+=2) {
-
+      newEvenArray.push(evenArray[i] * evenArray[i + 1]);
     }
+    return newEvenArray;
+  }
 
+  while (inputArray.length > 1) {
+    if (!(count % 2)) {
+      oddIteration(inputArray);
+      count++;
+    } else {
+      evenIteration(inputArray);
+      count++;
+    }
   }
 }
 
