efa243b71061798c8f9321dd90f87666b6ddf94a
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-23 22:28:19 -0700

    prob013 started

---
+++ prob013/arrayChange.ts
@@ -0,0 +1,9 @@
+// Goal to change array to ensure sequential order within array
+//   each change causes element to increase by one
+//
+
+function arrayChange(inputArray: number[]): number {
+
+
+}
+console.log(arrayChange([1, 1, 1])); // should return 3.... 1 for middle element, 2 for end element
