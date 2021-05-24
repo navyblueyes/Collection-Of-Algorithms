96601e4e87d36a5e568119cff70910fd58f26a77
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-23 23:28:43 -0700

    prob014 started

---
+++ prob014/arrayConversion.ts
@@ -0,0 +1,13 @@
+// Given -- array of 2000 integers... perform two operations
+//  1. odd iterations -- return sum of each pair of consecutive elements
+//  2. even iterations -- return product of each pair of consecutive elements
+// Returns an array of a single element
+//   [1,2,3,4,5,6,7,8]
+//    --> first iteration returns  [3, 7, 11, 15]
+//    --> second iteration returns [21, 165]
+//    --> third iteration returns  [186]
+function arrayConversion(inputArray: number[]): number[]{
+
+}
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
