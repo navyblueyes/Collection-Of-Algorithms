63099ceaae235ca4f0250a270cdda2672391b953
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-23 23:15:35 -0700

    prob013 finished

--- prob013/arrayChange.ts
+++ prob013/arrayChange.ts
@@ -4,6 +4,15 @@
 
 function arrayChange(inputArray: number[]): number {
 
+  let count = 0;
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] >= inputArray[i + 1]) {
+      const difference = (inputArray[i] + 1) - inputArray[i + 1];
+      inputArray[i + 1] = inputArray[i] + 1;
+      count += difference;
+    }
+  }
+  return count;
 }
 console.log(arrayChange([1, 1, 1])); // should return 3.... 1 for middle element, 2 for end element
