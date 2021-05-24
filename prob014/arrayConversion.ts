5ab826e5a6cd4949a8311158ab024e4f492c1a16
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-05-24 00:07:27 -0700

    prob014 finished

--- prob014/arrayConversion.ts
+++ prob014/arrayConversion.ts
@@ -10,31 +10,25 @@ function arrayConversion(inputArray: number[]): number[]{
   let count = 0;
 
   while (inputArray.length > 1) {
-    if (!(count % 2)) {
-      oddIteration(inputArray);
-      count++;
-    } else {
-      evenIteration(inputArray);
-      count++;
-    }
+    inputArray = runIteration(inputArray, count)
+    count++;
   }
   return inputArray
 }
 
 
-function oddIteration(oddArray: number[]): number[] {
-  let newOddArray = [];
-  for (let i = 0; i < oddArray.length; i+=2) {
-    newOddArray.push(oddArray[i] + oddArray[i + 1]);
+function runIteration(thisArray: number[], count: number): number[] {
+  let newArray = [];
+  if (!(count % 2)) {
+    for (let i = 0; i < thisArray.length; i += 2) {
+      newArray.push(thisArray[i] + thisArray[i + 1]);
+    }
+  } else {
+    for (let i = 0; i < thisArray.length; i += 2) {
+      newArray.push(thisArray[i] * thisArray[i + 1]);
+    }
   }
-  return newOddArray;
-}
-function evenIteration(evenArray: number[]): number[] {
-  let newEvenArray = [];
-  for (let i = 0; i < evenArray.length; i+=2) {
-    newEvenArray.push(evenArray[i] * evenArray[i + 1]);
-  }
-  return newEvenArray;
+  return newArray;
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
