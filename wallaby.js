5906e78be3840f4121431f2b8b4d19cd114b948f
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2022-01-29 23:34:58 -0600

    015; complete

---
+++ wallaby.js
@@ -0,0 +1,10 @@
+module.exports = function (wallaby) {
+  return {
+    files: ["src/**/*.ts*", "test/**/*.ts*", "!test/**/test-*.ts*"],
+    tests: ["test/**/test-*.ts*"],
+
+    env: {
+      type: "node",
+    },
+  };
+};
