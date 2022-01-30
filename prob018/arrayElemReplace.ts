792bb163e1489eb734fe043bc53832be7acccb06
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2022-01-30 00:57:55 -0600

    018; complete

---
+++ prob018/arrayElemReplace.ts
@@ -0,0 +1,19 @@
+// given an array, integer to search for, integer to replace searched with
+
+// example
+// [1,2,1], 1 ,3 ... generates.... [3,2,3]
+
+function arrayElemReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] == elemToReplace) {
+      inputArray[i] = substitutionElem;
+    }
+  }
+  return inputArray;
+}
+
+console.log(arrayElemReplace([1, 2, 1], 1, 3));
