f9e6b329f51e0ccfca729b4e7c47b8d5e9fbfb0d
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2022-04-12 00:39:03 -0500

    working on test for 1

--- prob001/index.spec.ts
+++ prob001/index.spec.ts
@@ -1,7 +1,33 @@
+import { isExportDeclaration } from "typescript";
 import arrayMinimization from "./index";
 
 describe(arrayMinimization.name, () => {
-  it('test 1', () => {
-    const test1 = arrayMinimization([(4, 7, 8, 9)]);
+  it("test 1", () => {
+    // run test
+    const test1 = arrayMinimization([4, 7, 8, 9]);
+
+    // assert
+    expect(test1).toBe(7);
   });
+
+
+  //    [1, 2, 4, 6, 7] -- 4
+  it("test 2", () => {
+    // run test
+    const test1 = arrayMinimization([1, 2, 4, 6, 7]);
+
+    // assert
+    expect(test1).toBe(4);
+  });
+
+
+  //    [5,  6,  7,  8] -- 6
+  it("test 3", () => {
+    // run test
+    const test1 = arrayMinimization([5, 6, 7, 8]);
+
+    // assert
+    expect(test1).toBe(6);
+  });
+
 });
--- prob001/index.ts
+++ prob001/index.ts
@@ -8,7 +8,7 @@
 //    [1, 2, 4, 6, 7] -- 4
 //    [5,  6,  7,  8] -- 6
 
-function arrayMinimization(a: number[]): number {
+export function arrayMinimization(a: number[]): number {
   const isEven = a.length % 2 === 0;
 
   return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
