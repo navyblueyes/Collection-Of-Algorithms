1239d90eaa5d1156edb1dceed72c642269b43d80
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2022-04-25 20:56:27 -0500

    educative 01 started

---
+++ educative/array-01-sliding-window/slidingWindow.ts
@@ -0,0 +1,15 @@
+// Given an integer array and a window of size `w`, find the current maximum value in the window as it slides through the entire array.
+
+// sample input
+//    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//    window_size = 3
+
+// expected output
+//
+//    [3, 4, 5, 6, 7, 8, 9, 10]
+
+let findMaxSlidingWindow = function (nums, windowSize) {
+  var result = [];
+  //Write your code
+  return result;
+};
